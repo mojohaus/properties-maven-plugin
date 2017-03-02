e31925d1f696b9b15d18d25c5596696a1e3feb15
@mykelalvis <mykel.alvis@gmail.com>
2017-03-01 23:43:32 -0600

    Delete non-working it test

--- src/it/SetSystemPropertiesMojo/prepare.groovy
+++
@@ -1,5 +0,0 @@
-import junit.framework.Assert;
-
-def props = System.getProperties()
-
-Assert.assertNull("No random prop", props["RandomProperty"] )
--- src/it/SetSystemPropertiesMojo/verify.groovy
+++
@@ -1,5 +0,0 @@
-import junit.framework.Assert;
-
-def props = System.getProperties()
-println props
-Assert.assertEquals("random prop", "RANDOMVALUE", props["RandomProperty"] )
