2190d17ee1737f6d01e2bedcf6dd13429eb517cc
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-07-27 23:57:50 +0200

    Site checkin for project Properties Maven Plugin

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.properties"},{"l":"org.codehaus.mojo.properties_maven_plugin"}];updateSearchResults();
